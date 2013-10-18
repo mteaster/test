ac7b67cf67da81c0b06eccfc46bfef3eae01c7d9
mteaster <mteaster@uwm.edu>
2013-10-18 12:39:49 -0500

    experiment

---
+++ test/Controllers/BandController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using test.Models;
+
+namespace test.Controllers
+{
+    public class BandController : Controller
+    {
+        private BandsContext db = new BandsContext();
+
+        //
+        // GET: /Band/AllBands/
+
+        [ChildActionOnly]
+        public ActionResult AllBands()
+        {
+            return PartialView("_BandsPartial", db.BandProfiles.ToList());
+        }
+    }
+}
---
+++ test/Models/BandModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace test.Models
+{
+    public class BandsContext : DbContext
+    {
+        public BandsContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<BandProfile> BandProfiles { get; set; }
+    }
+
+    [Table("BandProfile")]
+    public class BandProfile
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int BandId { get; set; }
+        public string BandName { get; set; }
+    }
+}
