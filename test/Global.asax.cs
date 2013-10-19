2f2f21fa77ab606f597830afc3aa99220cd7a562
mteaster <mteaster@uwm.edu>
2013-10-19 18:49:36 -0500

    vs

--- test/Global.asax.cs
+++ test/Global.asax.cs
@@ -6,6 +6,9 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity;
+using test.Models;
+using test.Config;
 
 namespace test
 {
