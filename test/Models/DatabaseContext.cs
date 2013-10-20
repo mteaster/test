fe94f484be8935c261d7896343c2ed13d926907b
mteaster <mteaster@uwm.edu>
2013-10-19 19:29:54 -0500

    oops

---
+++ test/Models/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace test.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+        public DbSet<BandProfile> BandProfiles { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
