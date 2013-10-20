8958f788bf616bb79d6ed3e0c539a14d52ea2247
mteaster <mteaster@uwm.edu>
2013-10-20 14:12:04 -0500

    ok

--- test/Models/BandModels.cs
+++ test/Models/BandModels.cs
@@ -37,17 +37,17 @@ namespace test.Models
     {
         public BandMembership() {}
 
-        public BandMembership(string BandName, int MemberId)
+        public BandMembership(string BandId, int MemberId)
         {
-            this.BandName = BandName;
+            this.BandId= BandId;
             this.MemberId = MemberId;
         }
 
         [Required]
         [Key]
         [Column(Order=0)]
-        public string BandName { get; set; }
-        [ForeignKey("BandName")]
+        public string BandId { get; set; }
+        [ForeignKey("BandId")]
         public virtual BandProfile BandProfile { get; set; }
 
         [Required]
