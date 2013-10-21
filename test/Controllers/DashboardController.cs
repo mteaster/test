25dae3301f8a433db4b29729b5943d727c967bd8
Srfigie <samfigie@gmail.com>
2013-10-21 15:19:11 -0500

    Merger Changes
    Mike you might need to rollback if this doesnt work...

---
+++ test/Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace band.Controllers
+{
+    public class DashboardController : Controller
+    {
+        //
+        // GET: /Dashboard/
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = getMsg();
+            return View();
+        }
+
+        public String getMsg()
+        {
+            return "Code will go here to query DB for messages";
+        }
+
+
+
+    }
+}
