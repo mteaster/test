50668164b115aa36bd4f08ace946ce71eb4368fa
mike <mteaster@uwm.edu>
2013-10-23 17:27:24 -0500

    pls work

--- test/App_Start/RouteConfig.cs
+++ test/App_Start/RouteConfig.cs
@@ -20,6 +20,12 @@ namespace test
             );
 
             routes.MapRoute(
+                name: "Join",
+                url: "Dashboard/{bandId}",
+                defaults: new { controller = "Dashboard", action = "Index", bandId = "" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}",
                 defaults: new { controller = "Home", action = "Index" }
