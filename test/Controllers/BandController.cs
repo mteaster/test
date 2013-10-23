9a47316bb8d7921ddbda0edb0e996531f175aca3
Garrett DeMeyer <gddemeyer@gmail.com>
2013-10-23 15:56:04 -0700

    Band Update Features

--- test/Controllers/BandController.cs
+++ test/Controllers/BandController.cs
@@ -280,7 +280,7 @@ namespace test.Controllers
                 {
                     database.Entry(bandProfile).State = EntityState.Modified;
                     database.SaveChanges();
-                    return View("Home");
+                    return View("About");
                 }
                 else
                 {
