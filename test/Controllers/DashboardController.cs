65d6f575a4c4a9344406328d0a893b93b0b2e555
Andrew Fuhr <adfuhr@uwm.edu>
2013-10-22 23:42:27 -0500

    forgot to add file

--- test/Controllers/DashboardController.cs
+++ test/Controllers/DashboardController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using test.Models;
 
 namespace band.Controllers
 {
@@ -15,6 +16,17 @@ namespace band.Controllers
         {
             ViewBag.Message = getMsg();
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Post(MessageBoardPost m)
+        {
+            if (ModelState.IsValid)
+            {
+                //code to post here ??
+                return RedirectToAction("Index");
+            }
+            return View("Index");
         }
 
         public String getMsg()
---
+++ test/Models/MessageBoardPost.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test.Models
+{
+    public class MessageBoardPost
+    {
+        public int ID { get; set; }
+        public int BandID { get; set; }
+        public string UserName { get; set; }
+        public DateTime PostTime { get; set; }
+        public string Content { get; set; }
+    }
+   
+}
