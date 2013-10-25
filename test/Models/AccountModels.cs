8b8bafa9e43c51c68e635e7ea3a1e4b3c2f7196a
mteaster <mteaster@uwm.edu>
2013-10-25 17:27:22 -0500

    use db context correctly

--- test/Models/AccountModels.cs
+++ test/Models/AccountModels.cs
@@ -23,26 +23,7 @@ namespace test.Models
         public string DisplayName { get; set; }
     }
 
-    public class LocalPasswordModel
-    {
-        [Required]
-        [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
-        public string OldPassword { get; set; }
-
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "New password")]
-        public string NewPassword { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
-    }
-
-    public class DisplayNameModel
+    public class ChangeDisplayNameModel
     {
         [Required]
         [Display(Name = "Display name")]
