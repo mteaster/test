449dfdcce8e2f84c5c3d9b32db3b4272d937319c
mike <mteaster@uwm.edu>
2013-10-28 11:16:39 -0500

    admin page

---
+++ test/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Security;
+using Microsoft.Web.WebPages.OAuth;
+using test.Models;
+using WebMatrix.WebData;
+using System.Data;
+
+namespace test.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (!Roles.IsUserInRole("Administrator"))
+            {
+                @ViewBag.StatusMessage = "you're not an admin, idiot";
+                return View("Error");
+            }
+
+            return View();
+        }
+    }
+}
