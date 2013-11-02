1fe550eadefe56403d01332bf573b1e5b2146c77
mteaster <mteaster@uwm.edu>
2013-11-02 15:53:05 -0500

    the button is just a button

--- test/Scripts/messageboard.js
+++ test/Scripts/messageboard.js
@@ -4,7 +4,7 @@ function edit(id)
 {
     console.log("editing post " + id);
 
-    var form = $('<form method="post" action="/Dashboard/EditPost?postId=' + id + '"><fieldset><legend>Post Message Form</legend><textarea class="text-box multi-line" data-val="true" id="Content" name="Content"></textarea><br><input value="Post" type="submit"><button onclick="cancel(' + id + ')">Cancel</button></fieldset></form>');
+    var form = $('<form method="post" action="/Dashboard/EditPost?postId=' + id + '"><fieldset><legend>Post Message Form</legend><textarea class="text-box multi-line" data-val="true" id="Content" name="Content"></textarea><br><input value="Edit" type="submit"><button type=button onclick="cancel(' + id + ')">Cancel</button></fieldset></form>');
 
     $("#content-" + id).hide();
     $("#post-" + id).append(form);
