7a3ee0e3d7c4ac9d4bf0d7a40ccb0921afa62ed0
mteaster <mteaster@uwm.edu>
2013-11-02 16:41:57 -0500

    log that

--- test/Scripts/messageboard.js
+++ test/Scripts/messageboard.js
@@ -5,6 +5,8 @@ function edit(id)
     console.log("editing post " + id);
 
     var content = $("post-content-" + id).text();
+
+    console.log(content);
     var form = $('<form id="post-edit-' + id + '" method="post" action="/Dashboard/EditPost?postId=' + id + '"><fieldset><legend>Post Message Form</legend><textarea class="text-box multi-line" data-val="true" id="Content" name="Content"></textarea><br><input value="Edit" type="submit"><button type=button onclick="cancel(' + id + ')">Cancel</button></fieldset></form>');
 
     $("#post-" + id).hide();
