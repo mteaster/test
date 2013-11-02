34cc57cdf19877311d3660f4c7ef993a1b375c96
mteaster <mteaster@uwm.edu>
2013-11-02 16:38:28 -0500

    y u not work?

--- test/Scripts/messageboard.js
+++ test/Scripts/messageboard.js
@@ -4,12 +4,11 @@ function edit(id)
 {
     console.log("editing post " + id);
 
-    var content = $("post-content-" + id).text();
     var form = $('<form id="post-edit-' + id + '" method="post" action="/Dashboard/EditPost?postId=' + id + '"><fieldset><legend>Post Message Form</legend><textarea class="text-box multi-line" data-val="true" id="Content" name="Content"></textarea><br><input value="Edit" type="submit"><button type=button onclick="cancel(' + id + ')">Cancel</button></fieldset></form>');
 
     $("#post-" + id).hide();
     $("#post-container-" + id).append(form);
-    $("#post-edit-" + id + " textarea").val(content);
+    $("#post-edit-" + id + " textarea").val($("post-content-" + id).text());
 }
 
 function cancel(id)
