67ae0df77d7373044d7e8becf523b0a7adfb6678
mteaster <mteaster@uwm.edu>
2013-11-02 18:21:54 -0500

    only edit one at a time

--- test/Scripts/messageboard.js
+++ test/Scripts/messageboard.js
@@ -4,8 +4,11 @@ function edit(id)
 {
     console.log("editing post " + id);
 
+    $(".post-edit").remove();
+    $(".post-content").show();
+
     var content = $("#post-content-" + id).text();
-    var form = $('<form id="post-edit-' + id + '" method="post" action="/Dashboard/EditPost?postId=' + id + '"><fieldset><legend>Post Message Form</legend><textarea class="text-box multi-line" data-val="true" id="Content" name="Content"></textarea><br><input value="Save" type="submit"><button type=button onclick="cancel(' + id + ')">Cancel</button></fieldset></form>');
+    var form = $('<form class="post-edit" id="post-edit-' + id + '" method="post" action="/Dashboard/EditPost?postId=' + id + '"><fieldset><legend>Post Message Form</legend><textarea class="text-box multi-line" data-val="true" id="Content" name="Content"></textarea><br><input value="Save" type="submit"><button type=button onclick="cancel(' + id + ')">Cancel</button></fieldset></form>');
 
     $("#post-" + id).hide();
     $("#post-container-" + id).append(form);
