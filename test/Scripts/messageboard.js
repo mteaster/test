be28dc2010b9428a950257d80c55b768b6e8f2c5
mteaster <mteaster@uwm.edu>
2013-11-02 16:35:52 -0500

    ok

--- test/Scripts/messageboard.js
+++ test/Scripts/messageboard.js
@@ -3,19 +3,19 @@ console.log("messageboard.js");
 function edit(id)
 {
     console.log("editing post " + id);
-    var content = $("#content-" + id);
-    var value = content.val();
-    var form = $('<form id="edit-' + id + '" method="post" action="/Dashboard/EditPost?postId=' + id + '"><fieldset><legend>Post Message Form</legend><textarea class="text-box multi-line" data-val="true" id="Content" name="Content"></textarea><br><input value="Edit" type="submit"><button type=button onclick="cancel(' + id + ')">Cancel</button></fieldset></form>');
 
-    content.hide();
-    $("#post-" + id).append(form);
-    $("#edit-" + id + " textarea").val(value);
+    var content = $("post-content-" + id).text();
+    var form = $('<form id="post-edit-' + id + '" method="post" action="/Dashboard/EditPost?postId=' + id + '"><fieldset><legend>Post Message Form</legend><textarea class="text-box multi-line" data-val="true" id="Content" name="Content"></textarea><br><input value="Edit" type="submit"><button type=button onclick="cancel(' + id + ')">Cancel</button></fieldset></form>');
+
+    $("#post-" + id).hide();
+    $("#post-container-" + id).append(form);
+    $("#post-edit-" + id + " textarea").val(content);
 }
 
 function cancel(id)
 {
     console.log("canceling edit of post " + id);
 
-    $("#edit-" + id).remove();
-    $("#content-" + id).show();
+    $("#post-edit-" + id).remove();
+    $("#post-" + id).show();
 }
