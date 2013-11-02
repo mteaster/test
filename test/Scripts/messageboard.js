782a4ca691b43c59c51310f8b09f6b92b5324afc
mteaster <mteaster@uwm.edu>
2013-11-02 15:48:55 -0500

    maybe this will work

--- test/Scripts/messageboard.js
+++ test/Scripts/messageboard.js
@@ -2,10 +2,18 @@ console.log("messageboard.js");
 
 function edit(id)
 {
-    console.log("edit post with id " + id);
+    console.log("editing post " + id);
 
-    var form = $("<p>form goes here</>");
+    var form = $('<form method="post" action="/Dashboard/EditPost?postId=' + id + '><fieldset><legend>Post Message Form</legend><textarea class="text-box multi-line" data-val="true" id="Content" name="Content"></textarea><br><input value="Post" type="submit"><button onclick="cancel(' + id + ')">Cancel</button></fieldset></form>');
 
     $("#content-" + id).hide();
     $("#post-" + id).append(form);
+}
+
+function cancel(id)
+{
+    console.log("canceling edit of post " + id);
+
+    $("#edit-" + id).remove();
+    $("#content-" + id).show();
 }
