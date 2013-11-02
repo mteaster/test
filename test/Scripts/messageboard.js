e3f4cdf324955264943f9b23374be1963d0441f6
mteaster <mteaster@uwm.edu>
2013-11-02 18:23:21 -0500

    do it right

--- test/Scripts/messageboard.js
+++ test/Scripts/messageboard.js
@@ -5,7 +5,7 @@ function edit(id)
     console.log("editing post " + id);
 
     $(".post-edit").remove();
-    $(".post-content").show();
+    $(".post-container").show();
 
     var content = $("#post-content-" + id).text();
     var form = $('<form class="post-edit" id="post-edit-' + id + '" method="post" action="/Dashboard/EditPost?postId=' + id + '"><fieldset><legend>Post Message Form</legend><textarea class="text-box multi-line" data-val="true" id="Content" name="Content"></textarea><br><input value="Save" type="submit"><button type=button onclick="cancel(' + id + ')">Cancel</button></fieldset></form>');
