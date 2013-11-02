258133b8ebb2c53927459f91be72d1c2c2cd826e
mteaster <mteaster@uwm.edu>
2013-11-02 16:18:55 -0500

    ok

--- test/Scripts/messageboard.js
+++ test/Scripts/messageboard.js
@@ -3,11 +3,12 @@ console.log("messageboard.js");
 function edit(id)
 {
     console.log("editing post " + id);
-
-    var form = $('<form id="edit-' + id + '" method="post" action="/Dashboard/EditPost?postId=' + id + '"><fieldset><legend>Post Message Form</legend><textarea class="text-box multi-line" data-val="true" id="Content" name="Content"></textarea><br><input value="Edit" type="submit"><button type=button onclick="cancel(' + id + ')">Cancel</button></fieldset></form>');
-
-    $("#content-" + id).hide();
+    var content = $("#content-" + id);
+    var form = $('<form id="edit-' + id + '" method="post" action="/Dashboard/EditPost?postId=' + id + '"><fieldset><legend>Post Message Form</legend><textarea class="text-box multi-line" data-val="true" id="Content" name="Content"></textarea id="text- ' + id + '"><br><input value="Edit" type="submit"><button type=button onclick="cancel(' + id + ')">Cancel</button></fieldset></form>');
+    
+    content.hide();
     $("#post-" + id).append(form);
+    $("#text-" + id).val(content.val());
 }
 
 function cancel(id)
