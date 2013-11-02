1c17394f67527593cd4e261503fc2fc295a60c91
mteaster <mteaster@uwm.edu>
2013-11-02 16:42:18 -0500

    i see it now

--- test/Scripts/messageboard.js
+++ test/Scripts/messageboard.js
@@ -4,14 +4,12 @@ function edit(id)
 {
     console.log("editing post " + id);
 
-    var content = $("post-content-" + id).text();
-
-    console.log(content);
+    var content = $("#post-content-" + id).text();
     var form = $('<form id="post-edit-' + id + '" method="post" action="/Dashboard/EditPost?postId=' + id + '"><fieldset><legend>Post Message Form</legend><textarea class="text-box multi-line" data-val="true" id="Content" name="Content"></textarea><br><input value="Edit" type="submit"><button type=button onclick="cancel(' + id + ')">Cancel</button></fieldset></form>');
 
     $("#post-" + id).hide();
     $("#post-container-" + id).append(form);
-    $("#post-edit-" + id + " textarea").text(content);
+    $("#post-edit-" + id + " textarea").val(content);
 }
 
 function cancel(id)
