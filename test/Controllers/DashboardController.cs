338b114c503993553dd409eb3c96fbf41597a44e
mteaster <mteaster@uwm.edu>
2013-11-03 16:06:19 -0600

    ok

--- test/Controllers/DashboardController.cs
+++ test/Controllers/DashboardController.cs
@@ -22,7 +22,7 @@ namespace band.Controllers
             if (MessageBoardUtil.DeletePost(postId))
             {
                 ViewBag.StatusMessage = "Post deleted!";
-                return RedirectToLocal(Request.UrlReferrer);
+                return RedirectToLocal(Request.UrlReferrer.ToString());
             }
             else
             {
