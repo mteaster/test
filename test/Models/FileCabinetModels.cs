2a9d0ff24818e459f1a349147bfaf9680143d08e
mike <mteaster@uwm.edu>
2013-11-06 11:43:48 -0600

    forgot these

---
+++ test/Models/FileCabinetModels.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Mvc;
+using test.Models.Account;
+using test.Models.Band;
+
+namespace test.Models.FileCabinet
+{
+    [Table("FileEntry")]
+    public class FileEntry
+    {
+        [Key]
+        [Required]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int FileId { get; set; }
+
+        [Required]
+        public string FileName { get; set; }
+
+        [Required]
+        public string FilePath { get; set; }
+
+        [Required]
+        public int BandId { get; set; }
+        [ForeignKey("BandId")]
+        public virtual BandProfile BandProfile { get; set; }
+
+        [Required]
+        public int UploaderId { get; set; }
+        [ForeignKey("UploaderId")]
+        public virtual UserProfile UploaderProfile { get; set; }
+    }
+}
