76a94d56ee40e2a3cde729ce0e7cf67c5015a6dd
mike <mteaster@uwm.edu>
2013-11-07 20:04:33 -0600

    disable admin creation

--- test/Controllers/HomeController.cs
+++ test/Controllers/HomeController.cs
@@ -58,9 +58,9 @@ namespace test.Controllers
         {
             ViewBag.StatusMessage = "what am i doing here";
             
-            WebSecurity.CreateUserAndAccount("admin", "password", new { DisplayName = "admin" });
-            Roles.CreateRole("Administrator");
-            Roles.AddUserToRole("admin", "Administrator");
+            //WebSecurity.CreateUserAndAccount("admin", "password", new { DisplayName = "admin" });
+            //Roles.CreateRole("Administrator");
+            //Roles.AddUserToRole("admin", "Administrator");
 
             return View();
         }
