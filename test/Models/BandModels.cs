01806344dfedd4b5e78135622e494a79c8908c38
mteaster <mteaster@uwm.edu>
2013-11-10 17:51:19 -0600

    some cleanup

--- test/Models/BandModels.cs
+++ test/Models/BandModels.cs
@@ -149,17 +149,6 @@ namespace test.Models.Band
         public string ConfirmPassword { get; set; }
     }
 
-    public class UpdateBandModel
-    {
-        [Display(Name = "New Band Name (leave blank to keep current name")]
-        public string NewBandName { get; set; }
-
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "New Password (leave blank to keep current password)")]
-        public string NewPassword { get; set; }
-    }
-
     public class MemberModel
     {
         [Required]
