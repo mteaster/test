93897abc483ec31509b3eaf25320b89020f313f3
mteaster <mteaster@uwm.edu>
2013-11-12 15:09:34 -0600

    work

--- test/Scripts/files.js
+++ test/Scripts/files.js
@@ -4,12 +4,15 @@ filesApp.controller('FilesController', function FilesController($scope, $http)
 {
     console.log("running controller");
 
+    $status.files = [];
+
     var filesUrl = '/FileCabinet/GetJson?bandId=' + bandId + '&groupId=' + groupId;
     $http({ method: 'GET', url: filesUrl }).
         success(function (data, status, headers, config)
         {
             console.log("success");
             console.log(data);
+            $status.files = data;
         }).
         error(function (data, status, headers, config)
         {
