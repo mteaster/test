7fd2c321fe9b48b7d8a62f64020df69e435dceb9
mteaster <mteaster@uwm.edu>
2013-12-07 18:02:38 -0600

    super.js

---
+++ test/Scripts/super.js
@@ -0,0 +1,61 @@
+var superApp = angular.module("super", []);
+
+superApp.controller('SortableController', function SortableController($scope, $http)
+{
+    $scope.data = [];
+
+    $scope.init = function (url)
+    {
+        $http({ method: 'GET', url: url }).success(function (data) { $scope.data = data; });
+    };
+
+    $scope.column = '';
+    $scope.descending = false;
+
+    $scope.sort = function (column)
+    {
+        if ($scope.column == column)
+        {
+            $scope.descending = !$scope.descending;
+        }
+        else
+        {
+            $scope.column = column;
+            $scope.descending = false;
+        }
+    };
+});
+
+superApp.controller('SearchableController', function SearchableController($scope, $http)
+{
+    $scope.data = [];
+    $scope.url = "";
+    $scope.criteria = "";
+
+    $scope.init = function (url)
+    {
+        $scope.url = url;
+    };
+
+    $scope.search = function ()
+    {
+        console.log($scope.criteria);
+        $http({ method: 'GET', url: $scope.url + '/' + $scope.criteria }).success(function (data) { $scope.data = data; });
+    };
+
+    $scope.column = '';
+    $scope.descending = false;
+
+    $scope.sort = function (column)
+    {
+        if ($scope.column == column)
+        {
+            $scope.descending = !$scope.descending;
+        }
+        else
+        {
+            $scope.column = column;
+            $scope.descending = false;
+        }
+    };
+});
